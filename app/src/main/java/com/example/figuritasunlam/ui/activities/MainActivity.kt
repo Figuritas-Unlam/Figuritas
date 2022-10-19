752527f86ae1fd217f8ccdc8c69dd62cc43e641b
eliaasgabriel <eliasruedac@gmail.com>
2022-10-19 17:44:50 -0300

    "Agrego packages para separar el proyecto en capas"

--- app/src/main/java/com/example/figuritasunlam/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/figuritasunlam/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.figuritasunlam
+package com.example.figuritasunlam.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.figuritasunlam.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
