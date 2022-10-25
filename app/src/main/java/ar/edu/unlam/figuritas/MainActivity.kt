8ea05ac28b8c1ca50552ec0522a70b5e4691a062
Tomás Aranda <tomas.aranda.95@gmail.com>
2022-10-25 20:42:25 -0300

    add OpenPackActivity launcher on MainActivity button

--- app/src/main/java/ar/edu/unlam/figuritas/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/figuritas/MainActivity.kt
@@ -1,11 +1,24 @@
 package ar.edu.unlam.figuritas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ar.edu.unlam.figuritas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonMainActivity.setOnClickListener {
+            val intent = Intent(this, OpenPackActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
