e537263b975821817a65da7b29030f2cb339cafe
aitorLob0 <aolaechealobo@alumno.unlam.edu.ar>
2022-10-27 14:26:54 -0300

    Add AppCenter Implementation

--- app/src/main/java/com/example/figuritasunlam/MainActivity.kt
+++ app/src/main/java/com/example/figuritasunlam/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.figuritasunlam
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "{Your app secret here}",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
