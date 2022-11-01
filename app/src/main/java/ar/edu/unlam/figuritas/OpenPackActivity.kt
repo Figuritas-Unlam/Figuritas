f1b3e9fe55b94eef34806828ed67dddbc344cd7a
Tomás Aranda <tomas.aranda.95@gmail.com>
2022-11-01 20:59:09 -0300

    change opening pack animations

--- app/src/main/java/ar/edu/unlam/figuritas/OpenPackActivity.kt
+++ app/src/main/java/ar/edu/unlam/figuritas/OpenPackActivity.kt
@@ -1,16 +1,20 @@
 package ar.edu.unlam.figuritas
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
-import androidx.core.animation.doOnEnd
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import ar.edu.unlam.figuritas.databinding.ActivityOpenPackBinding
 
 class OpenPackActivity : AppCompatActivity() {
-    private lateinit var firstCard: ImageView
-    private lateinit var secondCard: ImageView
-    private lateinit var thirdCard: ImageView
-    private lateinit var fourthCard: ImageView
-    private lateinit var fifthCard: ImageView
+    private lateinit var incomingCard: ImageView
+    private var currentCard: ImageView? = null
     private lateinit var binding: ActivityOpenPackBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,24 +22,42 @@ class OpenPackActivity : AppCompatActivity() {
         binding = ActivityOpenPackBinding.inflate(layoutInflater)
         val view = binding.root
 
-        firstCard = binding.imageView
-        secondCard = binding.imageView2
-        thirdCard = binding.imageView3
-        fourthCard = binding.imageView4
-        fifthCard = binding.imageView5
-        var currentCard = firstCard
-        var incomingCard = secondCard
+        incomingCard = binding.imageView1
+
         var height = 100f
+        var incomingCardNumber = 1
 
         binding.button.setOnClickListener {
-            currentCard.animate().setDuration(1000).x(600f).y(height)
+            currentCard?.animate()?.setDuration(1000)?.x(600f)?.y(height)
             incomingCard.animate().setDuration(1000).y(400f)
 
-            currentCard = secondCard
-            incomingCard = thirdCard
+            currentCard = incomingCard
+            updateViews(++incomingCardNumber)
             height += 100
         }
 
         setContentView(view)
     }
+
+    private fun updateViews(incomingCardNumber: Int) {
+        if (incomingCardNumber <= 5) {
+            incomingCard = binding.root.findViewById(resources.getIdentifier("imageView${incomingCardNumber}", "id", BuildConfig.APPLICATION_ID))
+        } else {
+            binding.button.visibility = View.GONE
+            for (cardNumber in 1..5) {
+                val cardView: ImageView = binding.root
+                    .findViewById(resources.getIdentifier("imageView${cardNumber}", "id", BuildConfig.APPLICATION_ID))
+                if (cardNumber == 5) {
+                    cardView.animate()
+                        .setDuration(1000)
+                        .setStartDelay((300*(cardNumber-1)).toLong())
+                        .x(20f)
+                        .y(500f)
+                } else {
+                    cardView.animate().setDuration(1000).setStartDelay((300*(cardNumber-1)).toLong()).x(10f*cardNumber).y(120f*(cardNumber+2))
+                }
+            }
+        }
+    }
+
 }
