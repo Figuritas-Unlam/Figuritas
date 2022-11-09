04b934c367b828004641924c5b42130eba2a876a
Pablo Carballo <pablojavier.carballo@mercadolibre.com>
2022-11-09 20:59:15 -0300

    PlayerRepository fix

---
+++ app/src/main/java/ar/edu/unlam/figuritas/repository/PlayerRepository.kt
@@ -0,0 +1,35 @@
+package ar.edu.unlam.figuritas.repository
+
+
+import ar.edu.unlam.figuritas.data.api.PlayerClient
+import ar.edu.unlam.figuritas.model.WorldCupTeamId
+import ar.edu.unlam.figuritas.model.response.*
+import retrofit2.Response
+
+class PlayerRepository(private val playerClient: PlayerClient) {
+
+    suspend fun getPlayerById(playerId : Int) : Response<PlayerResponse> {
+        return playerClient.searchPlayerById(playerId)
+    }
+
+    suspend fun getPlayersByCountry(country: WorldCupTeamId) : List<PlayerResponse?>? {
+        return getSquadDataByCountry(country)?.map { getPlayerById(it.playerId).body() }
+    }
+
+    suspend fun getRandomPlayers(qty: Int) : List<PlayerResponse?> {
+        return List(qty) { getPlayerById(getRandomPlayerId()).body() }
+    }
+
+    private suspend fun getRandomPlayerId() : Int {
+        return getSquadDataByCountry(getRandomCountryId())?.random()?.playerId!!
+    }
+
+    private suspend fun getSquadDataByCountry(country: WorldCupTeamId): List<SquadPlayerData>? {
+        return playerClient.searchPlayersByCountryId(country.id).body()?.data?.squad?.data
+    }
+
+
+    private fun getRandomCountryId() : WorldCupTeamId {
+        return WorldCupTeamId.values().random()
+    }
+}
