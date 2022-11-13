80d9970ce60796650f7422af87e210eac9695dba
eliaasgabriel <eliasruedac@gmail.com>
2022-11-12 21:21:35 -0300

    "Create AlbumActivity, AlbumViewModel y AlbumAdapter"

--- app/src/main/java/ar/edu/unlam/figuritas/ui/viewModel/AlbumViewModel.kt
+++ app/src/main/java/ar/edu/unlam/figuritas/ui/viewModel/AlbumViewModel.kt
@@ -9,5 +9,52 @@ import javax.inject.Inject
 @HiltViewModel
 class AlbumViewModel @Inject constructor() : ViewModel(){
 
+    private var listCountrys = mutableListOf<String>()
+    private var listPlayers = mutableListOf<PlayerEntity>()
 
+
+    fun setCountrys(){
+
+        listCountrys.add("Qatar")
+        listCountrys.add("Ecuador")
+        listCountrys.add("Senegal")
+        listCountrys.add("Netherlands")
+
+        listCountrys.add("England")
+        listCountrys.add("IR Iran")
+        listCountrys.add("USA")
+        listCountrys.add("Wales")
+
+        listCountrys.add("Argentina")
+        listCountrys.add("Saudi Arabia")
+        listCountrys.add("Mexico")
+        listCountrys.add("Poland")
+
+        listCountrys.add("France")
+        listCountrys.add("Australia")
+        listCountrys.add("Denmark")
+        listCountrys.add("Tunisia")
+
+        listCountrys.add("Spain")
+        listCountrys.add("Costa Rica")
+        listCountrys.add("Germany")
+        listCountrys.add("Japan")
+
+        listCountrys.add("Belgium")
+        listCountrys.add("Canada")
+        listCountrys.add("Morocco")
+        listCountrys.add("Croatia")
+
+        listCountrys.add("Brazil")
+        listCountrys.add("Serbia")
+        listCountrys.add("Switzerland")
+        listCountrys.add("Cameroon")
+
+        listCountrys.add("Portugal")
+        listCountrys.add("Ghana")
+        listCountrys.add("Uruguay")
+        listCountrys.add("Korea Republic")
+
+
+    }
 }
