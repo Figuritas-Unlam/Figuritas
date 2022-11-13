d013de92a9fb2cd6f61943e596f949d0d23ae25b
eliaasgabriel <eliasruedac@gmail.com>
2022-11-12 21:21:04 -0300

    "Create AlbumActivity, AlbumViewModel y AlbumAdapter"

---
+++ app/src/main/java/ar/edu/unlam/figuritas/ui/adapter/AlbumAdapter.kt
@@ -0,0 +1,25 @@
+package ar.edu.unlam.figuritas.ui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ar.edu.unlam.figuritas.databinding.ItemSeleccionBinding
+
+class AlbumAdapter(
+
+) : RecyclerView.Adapter<SeleccionViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SeleccionViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SeleccionViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class SeleccionViewHolder(val binding:ItemSeleccionBinding) : RecyclerView.ViewHolder(binding.root)
---
+++ app/src/main/java/ar/edu/unlam/figuritas/ui/viewModel/AlbumViewModel.kt
@@ -0,0 +1,13 @@
+package ar.edu.unlam.figuritas.ui.viewModel
+
+import androidx.lifecycle.ViewModel
+import ar.edu.unlam.figuritas.model.entities.PlayerEntity
+import ar.edu.unlam.figuritas.model.response.PlayerResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AlbumViewModel @Inject constructor() : ViewModel(){
+
+
+}
