b78b20232e3384c1c99bb37c126f327871aad208
Tomás Aranda <tomas.aranda.95@gmail.com>
2022-11-15 23:00:35 -0300

    add Swaps Activity

---
+++ app/src/main/java/ar/edu/unlam/figuritas/ui/activities/SwapsActivity.kt
@@ -0,0 +1,12 @@
+package ar.edu.unlam.figuritas.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ar.edu.unlam.figuritas.R
+
+class SwapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_swaps)
+    }
+}
